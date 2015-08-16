9dcbabd5715a690c4acc00d946d14358eb53a80c
Shaul Lahav <shaul.lahav@bidflyer.net>
2015-08-16 21:56:32 +0300

    initial commit

--- build.sbt
+++ build.sbt
@@ -7,9 +7,9 @@ scalaVersion := "2.11.6"
 scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
 
 libraryDependencies ++= Seq(
-  "com.typesafe.slick" %% "slick" % "3.0.0-RC2",
-  "com.typesafe.slick" %% "slick-codegen" % "3.0.0-RC2",
-  "mysql" % "mysql-connector-java" % "5.1.34",
+  "com.typesafe.slick" %% "slick" % "3.0.0",
+  "com.typesafe.slick" %% "slick-codegen" % "3.0.0",
+  "mysql" % "mysql-connector-java" % "5.1.35",
   "com.zaxxer" % "HikariCP" % "2.3.2",
   "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 )
@@ -23,8 +23,8 @@ lazy val slickCodeGenTask = (sourceManaged, dependencyClasspath in Compile, runn
   val outputDir = (dir / "main/slick").getPath
   val username = "user"
   val password = "password"
-  val url = "jdbc:mysql://localhost/slick"
-val jdbcDriver = "com.mysql.jdbc.Driver"
+  val url = "jdbc:mysql://localhost:3306/test"
+  val jdbcDriver = "com.mysql.jdbc.Driver"
   val slickDriver = "slick.driver.MySQLDriver"
   val pkg = "me.arnaudtanguy.models"
   toError(r.run("slick.codegen.SourceCodeGenerator", cp.files, Array(slickDriver, jdbcDriver, url, outputDir, pkg, username, password), s.log))
--- src/main/resources/db/Schema.sql
+++ src/main/resources/db/Schema.sql
@@ -1,8 +1,27 @@
-DROP TABLE IF EXISTS `action`;
-CREATE TABLE `action` (
-  `id`   BIGINT(20)   NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+DROP TABLE IF EXISTS company;
+CREATE TABLE company (
+  company_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  company_name VARCHAR(255) NOT NULL,
+  owner_id BIGINT(20) NOT NULL,
+  created_time DATETIME NOT NULL,
+  public_id VARCHAR(255) NOT NULL,
+  
+  PRIMARY KEY (company_id)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS company_user;
+CREATE TABLE company_user (
+  user_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  company_id BIGINT(20) NOT NULL,
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  blocked BOOLEAN NOT NULL,
+  
+  PRIMARY KEY (user_id),
+  UNIQUE KEY (email, company_id),
+  FOREIGN KEY (company_id) REFERENCES company(company_id)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+ALTER TABLE company ADD CONSTRAINT fk_owner_user_id FOREIGN KEY (owner_id) REFERENCES company_user(user_id);
